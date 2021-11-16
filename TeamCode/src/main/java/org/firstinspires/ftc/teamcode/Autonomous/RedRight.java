c1e6f6450a5630d63df0313be04cf47618d89ce7
gkoh1 <75505740+gkoh1@users.noreply.github.com>
2021-11-16 08:53:53 -0600

    Update RedRight.java
    Changed pathing so we drop off in front, along with some other minor changes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedRight.java
@@ -25,14 +25,14 @@ import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Claw;
 import org.firstinspires.ftc.teamcode.subsystems.Pipeline;
 
-@Autonomous(name = "RedRight", group = "chad")
+@Autonomous(name="RedRight", group="chad")
 public class RedRight extends LinearOpMode {
-
+    
     OpenCvCamera webcam;
     Pipeline pipeline;
-
+    
     //Holds analysis
-    CupPosition cupPos;
+   CupPosition cupPos;
     //
     DcMotor frontleft;
     DcMotor frontright;
@@ -40,33 +40,33 @@ public class RedRight extends LinearOpMode {
     DcMotor backright;
     Arm arm;
     Claw claw;
-
+    
     //28 * 20 / (2ppi * 4.125)
     Double width = 12.0; //inches
     Integer cpr = 28; //counts per rotation
     Integer gearratio = 40;
     Double diameter = 4.125;
-    Double cpi = (cpr * gearratio) / (Math.PI * diameter); //counts per inch, 28cpr * gear ratio / (2 * pi * diameter (in inches, in the center))
+    Double cpi = (cpr * gearratio)/(Math.PI * diameter); //counts per inch, 28cpr * gear ratio / (2 * pi * diameter (in inches, in the center))
     Double bias = .717; //default 0.8
     Double meccyBias = .795; //change to adjust only strafing movement
     //
     Double conversion = cpi * bias;
     Boolean exit = false;
     //
-
+    
     BNO055IMU imu;
     Orientation angles;
     Acceleration gravity;
     //
-
-    //Stores possible analysis results from pipeline   
-    public enum CupPosition {
+ 
+ //Stores possible analysis results from pipeline   
+     public enum CupPosition {
         LEFT, // A
         MIDDLE, // B
         RIGHT // C
     }
 
-    public void runOpMode() {
+    public void runOpMode(){
         //
         initGyro();
         //
@@ -74,51 +74,51 @@ public class RedRight extends LinearOpMode {
         frontright = hardwareMap.dcMotor.get("frontRightDrive");
         backleft = hardwareMap.dcMotor.get("backLeftDrive");
         backright = hardwareMap.dcMotor.get("backRightDrive");
-
-        arm = new Arm(Arm.armRunMode.AUTONOMOUS, this, hardwareMap, telemetry);
-        claw = new Claw(hardwareMap);
+        
+      arm = new Arm(Arm.armRunMode.AUTONOMOUS, this, hardwareMap, telemetry);
+      claw = new Claw(hardwareMap);
 
 
         frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backleft.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        //Turns on camera   
-        initCV();
-
+        backleft.setDirection(DcMotorSimple.Direction.REVERSE); 
+     
+     //Turns on camera   
+         initCV();
+         telemetry.addLine("Ready and waiting!");
+         telemetry.update();
 
 
         //
         waitForStart();
-
+        
+        //
+        //
+        // BEGIN MOVEMENT
+        //
+        //
+        
         //Grasps starting block 
-        claw.closeClaw();
-
+        claw.closeClaw();  
+        
         //Gets analysis from pipeline 
         findCup();
 
         //Shows analysis in telemetry (this is mostly a testing thing)
         telemetry.addData("Position", cupPos);
-
+        telemetry.addData("Right avg", pipeline.box3_average_hue);
+        telemetry.addData("Middle avg", pipeline.box2_average_hue);
+        telemetry.addData("Left avg", pipeline.box1_average_hue);
         telemetry.update();
-        //
-
-
 
         //Moves arm motor to a certain position based on where the cup is 
-
         if (cupPos == CupPosition.LEFT) {
-            arm.autoPositions(1);
+            arm.autoPositions(1); // Bottom
         } else if (cupPos == CupPosition.MIDDLE) {
-            arm.autoPositions(2);
+            arm.autoPositions(2); // Middle
         } else if (cupPos == CupPosition.RIGHT) {
-            arm.autoPositions(2);
+            arm.autoPositions(3); // Top
         }
-
-
-
-
-
-
+        telemetry.update();
 
 
         //Motion via CHAD
@@ -128,87 +128,102 @@ public class RedRight extends LinearOpMode {
         //
         webcam.stopStreaming();
         webcam.closeCameraDevice();
-        strafeToPosition(-18, 0.2);
+        strafeToPosition(-17, 0.2);
         //
 
         //
         if (cupPos == CupPosition.LEFT) {
-            moveToPosition(21.75, 0.2);
+            moveToPosition(21.25, 0.2);
             claw.openClaw();
             moveToPosition(-6, 0.2);
             turnWithGyro(90, 0.2);
         } else if (cupPos == CupPosition.MIDDLE) {
-            moveToPosition(22.5, 0.2);
+            moveToPosition(22.25, 0.2);
             claw.openClaw();
             sleep(250);
             moveToPosition(-6, 0.2);
-            turnWithGyro(90, 0.2);
+            turnWithGyro(90,.2);
         } else if (cupPos == CupPosition.RIGHT) {
-            moveToPosition(12, 0.2);
-            turnWithGyro(180, 0.2);
+            moveToPosition(25.5, 0.2);
+         /*   turnWithGyro(180, 0.2);
+            sleep(750);
             arm.autoPositions(3);
-            moveToPosition(-15, 0.2);
-
+            moveToPosition(-7, 0.3);
+                  moveToPosition(-7.2, 0.1);
+*/
+            arm.autoPositions(4);
             claw.openClaw();
             sleep(250);
-            turnWithGyro(270, 0.2);
+            arm.autoPositions(3);
+            moveToPosition(-3, 0.2);
+            turnWithGyro(90, 0.2);
         }
         //
 
         //
-        strafeToPosition(33, 0.4);
+        strafeToPosition(25, 0.5);
         //
-        moveToPosition(60, 0.4);
+        moveToPosition(60, 0.5);
         //
+        strafeToPosition(-30, 0.5);
+        //
+        arm.autoPositions(5);
     }
+    
+ 
 
-
-
-    //Function to turn on camera
-    private void initCV() {
-        // Sets variable for the camera id
+//Function to turn on camera
+ private void initCV() {
+     // Sets variable for the camera id
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        // Gives a name to the webcam
+      // Gives a name to the webcam
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
         // Combines the above to create a webcam that we will use
         webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
         //Sets our pipeline to view images through as the one we want
-        pipeline = new Pipeline(125, 165, 80, -5); //Pipeline(first x, first y, shift x for a new box, shift y for a new box)
+        pipeline = new Pipeline(125, 185, 80, 3);
         webcam.setPipeline(pipeline);
 
-        // Turns on the webcam
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+// Turns on the webcam
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
             @Override
-            public void onOpened() {
-                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            public void onOpened()
+            {
+                webcam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
             }
             //This is needed so it knows what to do if something goes wrong
-            public void onError(int thing) {
-                telemetry.addData("error", thing);
+            public void onError(int thing){
+                telemetry.addData("error",thing );
             }
-
+            
         });
 
     }
-
-
-    private void findCup() {
+    
+    
+     private void findCup() {
 
         //Takes some time so we can run everything through the pipeline
-        sleep(3000);
+        sleep(3500);
 
         Pipeline.Position cupPosition = pipeline.position;
-
+        
         //Sets cupPos to a corresponding position basesd on pipeline analysis
 
-        if (cupPosition == Pipeline.Position.LEFT) {
+        if (cupPosition == Pipeline.Position.LEFT)
+        {
             cupPos = CupPosition.LEFT;
 
-        } else if (cupPosition == Pipeline.Position.RIGHT) {
+        }
+       else if (cupPosition == Pipeline.Position.RIGHT) 
+        {
             cupPos = CupPosition.RIGHT;
 
-        } else if (cupPosition == Pipeline.Position.MIDDLE) {
-            cupPos = CupPosition.MIDDLE;
+        }
+        else if (cupPosition == Pipeline.Position.MIDDLE) 
+        {
+           cupPos = CupPosition.MIDDLE;
 
         }
 
@@ -220,9 +235,9 @@ public class RedRight extends LinearOpMode {
     This function's purpose is simply to drive forward or backward.
     To drive backward, simply make the inches input negative.
      */
-    public void moveToPosition(double inches, double speed) {
+    public void moveToPosition(double inches, double speed){
         //
-        int move = (int)(Math.round(inches * conversion));
+        int move = (int)(Math.round(inches*conversion));
         //
         backleft.setTargetPosition(backleft.getCurrentPosition() + move);
         frontleft.setTargetPosition(frontleft.getCurrentPosition() + move);
@@ -239,8 +254,8 @@ public class RedRight extends LinearOpMode {
         frontright.setPower(speed);
         backright.setPower(speed);
         //
-        while (frontleft.isBusy() && frontright.isBusy() && backleft.isBusy() && backright.isBusy()) {
-            if (exit) {
+        while (frontleft.isBusy() && frontright.isBusy() && backleft.isBusy() && backright.isBusy()){
+            if (exit){
                 frontright.setPower(0);
                 frontleft.setPower(0);
                 backright.setPower(0);
@@ -259,10 +274,10 @@ public class RedRight extends LinearOpMode {
     This function uses the Expansion Hub IMU Integrated Gyro to turn a precise number of degrees (+/- 5).
     Degrees should always be positive, make speedDirection negative to turn left.
      */
-    public void turnWithGyro(double degrees, double speedDirection) {
+    public void turnWithGyro(double degrees, double speedDirection){
         //<editor-fold desc="Initialize">
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        double yaw = -angles.firstAngle; //make this negative
+        angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        double yaw = -angles.firstAngle;//make this negative
         telemetry.addData("Speed Direction", speedDirection);
         telemetry.addData("Yaw", yaw);
         telemetry.update();
@@ -274,22 +289,22 @@ public class RedRight extends LinearOpMode {
         double second;
         //</editor-fold>
         //
-        if (speedDirection > 0) { //set target positions
+        if (speedDirection > 0){//set target positions
             //<editor-fold desc="turn right">
-            if (degrees > 10) {
+            if (degrees > 10){
                 first = (degrees - 10) + devertify(yaw);
                 second = degrees + devertify(yaw);
-            } else {
+            }else{
                 first = devertify(yaw);
                 second = degrees + devertify(yaw);
             }
             //</editor-fold>
-        } else {
+        }else{
             //<editor-fold desc="turn left">
-            if (degrees > 10) {
+            if (degrees > 10){
                 first = devertify(-(degrees - 10) + devertify(yaw));
                 second = devertify(-degrees + devertify(yaw));
-            } else {
+            }else{
                 first = devertify(yaw);
                 second = devertify(-degrees + devertify(yaw));
             }
@@ -298,13 +313,13 @@ public class RedRight extends LinearOpMode {
         }
         //
         //<editor-fold desc="Go to position">
-        Double firsta = convertify(first - 5); //175
-        Double firstb = convertify(first + 5); //-175
+        Double firsta = convertify(first - 5);//175
+        Double firstb = convertify(first + 5);//-175
         //
         turnWithEncoder(speedDirection);
         //
         if (Math.abs(firsta - firstb) < 11) {
-            while (!(firsta < yaw && yaw < firstb) && opModeIsActive()) { //within range?
+            while (!(firsta < yaw && yaw < firstb) && opModeIsActive()) {//within range?
                 angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
                 gravity = imu.getGravity();
                 yaw = -angles.firstAngle;
@@ -313,9 +328,9 @@ public class RedRight extends LinearOpMode {
                 telemetry.addData("first after", convertify(first));
                 telemetry.update();
             }
-        } else {
+        }else{
             //
-            while (!((firsta < yaw && yaw < 180) || (-180 < yaw && yaw < firstb)) && opModeIsActive()) { //within range?
+            while (!((firsta < yaw && yaw < 180) || (-180 < yaw && yaw < firstb)) && opModeIsActive()) {//within range?
                 angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
                 gravity = imu.getGravity();
                 yaw = -angles.firstAngle;
@@ -326,13 +341,13 @@ public class RedRight extends LinearOpMode {
             }
         }
         //
-        Double seconda = convertify(second - 5); //175
-        Double secondb = convertify(second + 5); //-175
+        Double seconda = convertify(second - 5);//175
+        Double secondb = convertify(second + 5);//-175
         //
         turnWithEncoder(speedDirection / 3);
         //
         if (Math.abs(seconda - secondb) < 11) {
-            while (!(seconda < yaw && yaw < secondb) && opModeIsActive()) { //within range?
+            while (!(seconda < yaw && yaw < secondb) && opModeIsActive()) {//within range?
                 angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
                 gravity = imu.getGravity();
                 yaw = -angles.firstAngle;
@@ -341,7 +356,7 @@ public class RedRight extends LinearOpMode {
                 telemetry.addData("second after", convertify(second));
                 telemetry.update();
             }
-            while (!((seconda < yaw && yaw < 180) || (-180 < yaw && yaw < secondb)) && opModeIsActive()) { //within range?
+            while (!((seconda < yaw && yaw < 180) || (-180 < yaw && yaw < secondb)) && opModeIsActive()) {//within range?
                 angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
                 gravity = imu.getGravity();
                 yaw = -angles.firstAngle;
@@ -371,7 +386,7 @@ public class RedRight extends LinearOpMode {
     This function uses the encoders to strafe left or right.
     Negative input for inches results in left strafing.
      */
-    public void strafeToPosition(double inches, double speed) {
+    public void strafeToPosition(double inches, double speed){
         //
         int move = (int)(Math.round(inches * cpi * meccyBias));
         //
@@ -390,7 +405,7 @@ public class RedRight extends LinearOpMode {
         frontright.setPower(speed);
         backright.setPower(speed);
         //
-        while (frontleft.isBusy() && frontright.isBusy() && backleft.isBusy() && backright.isBusy()) {}
+        while (frontleft.isBusy() && frontright.isBusy() && backleft.isBusy() && backright.isBusy()){}
         frontright.setPower(0);
         frontleft.setPower(0);
         backright.setPower(0);
@@ -403,18 +418,18 @@ public class RedRight extends LinearOpMode {
     These functions are used in the turnWithGyro function to ensure inputs
     are interpreted properly.
      */
-    public double devertify(double degrees) {
-        if (degrees < 0) {
+    public double devertify(double degrees){
+        if (degrees < 0){
             degrees = degrees + 360;
         }
         return degrees;
     }
-    public double convertify(double degrees) {
-        if (degrees > 179) {
+    public double convertify(double degrees){
+        if (degrees > 179){
             degrees = -(360 - degrees);
-        } else if (degrees < -180) {
+        } else if(degrees < -180){
             degrees = 360 + degrees;
-        } else if (degrees > 360) {
+        } else if(degrees > 360){
             degrees = degrees - 360;
         }
         return degrees;
@@ -424,13 +439,13 @@ public class RedRight extends LinearOpMode {
     This function is called at the beginning of the program to activate
     the IMU Integrated Gyro.
      */
-    public void initGyro() {
+    public void initGyro(){
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
         //parameters.calibrationDataFile = "GyroCal.json"; // see the calibration sample opmode
-        parameters.loggingEnabled = true;
-        parameters.loggingTag = "IMU";
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
         //
         imu = hardwareMap.get(BNO055IMU.class, "imu");
@@ -441,7 +456,7 @@ public class RedRight extends LinearOpMode {
     This function is used in the turnWithGyro function to set the
     encoder mode and turn. Hi Leena :) 101
      */
-    public void turnWithEncoder(double input) {
+    public void turnWithEncoder(double input){
         frontleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
