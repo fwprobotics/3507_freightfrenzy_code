3e206356f666c25cdcdd72baf4f6ecbd106d9937
gkoh1 <75505740+gkoh1@users.noreply.github.com>
2022-01-25 15:44:22 -0600

    Update Lift.java
    Applied some stuff we use in the arm motor code to make this more subsystem-y, easy to change, and close to what we want this to ultimately be (namely with lots of easy automated control). Changes will definitely need to happen when we actually get the lift.

--- teamcode/subsystems/Lift.java
+++ teamcode/subsystems/Lift.java
@@ -12,13 +12,25 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class Lift{
 
     public DcMotor leftLiftMotor;
-    public DcMotor rightLiftMotor;
+    public DcMotor rightLiftMotor; //Not exactly sure what to do with this
     public LinearOpMode l;
     public Telemetry realTelemetry;
+    public double basePos
+    public double bottomPos;
+    public double middlePos;
+    public double topPos;
+    
 
     public enum liftRunMode {
         AUTONOMOUS,
         TELEOP
+    }
+    
+    public enum dropoffOptions {
+        BASE,
+        BOTTOM,
+        MIDDLE,
+        TOP
     }
 
     public Lift(liftRunMode runmode, LinearOpMode Input, HardwareMap hardwareMap, Telemetry telemetry){
@@ -31,31 +43,69 @@ public class Lift{
 
         // Different motor configurations depending on use case
         if (runmode.equals(liftRunMode.AUTONOMOUS)){
-            leftLiftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // This wont work rn with current encoder setup
+            leftLiftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Hopefully switch all these to run using encoder
             leftLiftMotor.setTargetPosition(0);
             leftLiftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         }
 
         if (runmode.equals(liftRunMode.TELEOP)){
             leftLiftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            leftLiftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            leftLiftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); 
             leftLiftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
+            
             rightLiftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             rightLiftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         }
 
     }
 
-    public void setPosition(int position){
-        leftLiftMotor.setTargetPosition(position);
-        leftLiftMotor.setPower(1);
+    public void setPosition(dropoffOptions position){
+        switch position {
+            case BASE:
+                leftLiftMotor.setTargetPosition(basePos); // Probably zero
+                break;
+            case BOTTOM:
+                leftLiftMotor.setTargetPosition(bottomPos);
+                break;
+            case MIDDLE:
+                leftLiftMotor.setTargetPosition(middlePos);
+                break;
+            case TOP:
+                leftLiftMotor.setTargetPosition(topPos);
+                break;
+        }
+        leftLiftMotor.setPower(1); //1 seems like a lot but we'll see
         leftLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    public void teleOpControl(double input){
-        leftLiftMotor.setPower(input);
-        rightLiftMotor.setPower(input);
+    public void teleOpControl(double input, boolean up, boolean mid, boolean down, boolean base){ // Rename inputs based on real buttons we choose
+       if down {
+            leftLiftMotor.setTargetPosition(bottomPos);
+            leftLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION); 
+            leftLiftMotor.setPower(1);
+                
+            
+        }
+          if up {
+            armMotor.setTargetPosition(topPos);
+            leftLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION); 
+            leftLiftMotor.setPower(1);
+        }
+        if mid{
+            armMotor.setTargetPosition(middlePos);
+            leftLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION); 
+            leftLiftMotor.setPower(1);
+        }
+        
+        if base {
+            armMotor.setTargetPosition(basePos);
+            leftLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION); 
+            leftLiftMotor.setPower(1);
+        }
+        if (!down &! up &! mid &! base &! armMotor.isBusy()){
+        leftLiftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftLiftMotor.setPower(input); //Probably should/can get toned down
+        }
     }
 
 
